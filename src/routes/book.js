17fcf8da258de2a966bbc540c882f8e93f94b1d2
Leonardo Almeida <leoalmeida.pessoal@gmail.com>
2023-07-10 18:22:27 -0300

    feat: create routes

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,5 +1,5 @@
-const route = "/item"
-const api = require("../api/item")
+const route = "/book"
+const api = require("../api/book")
 const auth = require("../api/auth")
 
 module.exports = (router) => {
@@ -7,5 +7,7 @@ module.exports = (router) => {
   router.route(`${route}/search`).all(auth.validate).post(api.search)
   router.route(`${route}/search/by-id`).all(auth.validate).get(api.byId)
   router.route(`${route}/search/by-name`).all(auth.validate).get(api.byName)
+  router.route(`${route}/search/by-gender`).all(auth.validate).get(api.byGender)
+  router.route(`${route}/search/by-author`).all(auth.validate).get(api.byAuthor)
   router.route(`${route}/:id`).all(auth.validate).get(api.getById).patch(api.update).delete(api.remove)
 }
---
+++ src/routes/loan.js
@@ -0,0 +1,12 @@
+const rout = "/loan"
+const api = require("../api/loan")
+const auth = require("../api/auth")
+
+module.exports = (router) => {
+  router.route(route).all(auth.validate).get(api.get).post(api.insert)
+  router.route(`${route}/search`).all(auth.validate).post(api.search)
+  router.route(`${route}/search/by-id`).all(auth.validate).get(api.byId)
+  router.route(`${route}/by-id-user`).all(auth.validate).get(api.getById)
+  router.route(`${route}/by-id-book`).all(auth.validate).get(api.get)
+  router.route(`${route}/:id`).all(auth.validate).get(api.getById).patch(api.update).delete(api.remove)
+}
