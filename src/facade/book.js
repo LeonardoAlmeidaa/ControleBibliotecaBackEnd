a01d3ce1695d7ff3cef16864c85616b281066a30
Leonardo Almeida <leoalmeida.pessoal@gmail.com>
2023-09-29 17:01:10 -0300

    CHORE: FACADE

--- src/facade/book.js
+++ src/facade/book.js
@@ -3,94 +3,94 @@ const validation = require("../model/book")
 const tableName = "book"
 
 const get = async () => {
-    return await dbo.get(tableName)
+  return await dbo.get(tableName)
 }
 
 const getById = async (id) => {
-    if (!id) {
-        return false
-    }
-    return await dbo.getById(id, tableName)
+  if (!id) {
+    return false
+  }
+  return await dbo.getById(id, tableName)
 }
 
 const search = async (body) => {
-    if (!body || !Array.isArray(body) || body.length === 0 ) {
-        return false
-    }
-    return await dbo.search(tableName, body)
+  if (!body || !Array.isArray(body) || body.length === 0) {
+    return false
+  }
+  return await dbo.search(tableName, body)
 }
 
 const byId = async (body) => {
-    if (!body) {
-        return false
-    }
-    return await dbo.byId(tableName, body, "id")
+  if (!body) {
+    return false
+  }
+  return await dbo.byId(tableName, body, "id")
 }
 
 const byName = async (body) => {
-    if (!body) {
-        return false
-    }
-    return await dbo.byName(tableName, body, "name")
+  if (!body) {
+    return false
+  }
+  return await dbo.byName(tableName, body, "name")
 }
 
 const byGender = async (body) => {
-    if (!body) {
-        return false
-    }
-    return await dbo.byGender(tableName, body, "gender")
+  if (!body) {
+    return false
+  }
+  return await dbo.byGender(tableName, body, "gender")
 }
 
 const byAuthor = async (body) => {
-    if (!body) {
-        return false
-    }
-    return await dbo.byAuthor(tableName, body, "author")
+  if (!body) {
+    return false
+  }
+  return await dbo.byAuthor(tableName, body, "author")
 }
 
 const insert = async (object) => {
-    try {
-        await validation.object.validateAsync(object, {
-            abortEarly: false
-        })
-    } catch (error) {
-        const errors = error.details.map((el) => el.message)
-        return { errors }
-    }
-    return await dbo.insert(object, tableName)
+  try {
+    await validation.object.validateAsync(object, {
+      abortEarly: false
+    })
+  } catch (error) {
+    const errors = error.details.map((el) => el.message)
+    return { errors }
+  }
+  return await dbo.insert(object, tableName)
 }
 
 const update = async (object, id) => {
-    if (!id) {
-        return false
-    }
-    try {
-        await validation.object.validateAsync(object, {
-            abortEarly: false
-        })
-    } catch (error) {
-        const errors = error.details.map((el) => el.message)
-        return { errors }
-    }
-    return await dbo.update(object, tableName)
+  if (!id) {
+    return false
+  }
+  try {
+    await validation.object.validateAsync(object, {
+      abortEarly: false
+    })
+  } catch (error) {
+    const errors = error.details.map((el) => el.message)
+    return { errors }
+  }
+  return await dbo.update(object, id, tableName)
 }
 
 const remove = async (id) => {
-    if(!id) {
-        return false
-    }
-    return await dbo.remove(id, tableName)
+  if (!id) {
+    return false
+  }
+  return await dbo.remove(id, tableName)
 }
 
 module.exports = {
-    get,
-    getById,
-    search,
-    byId,
-    byName,
-    byGender,
-    byAuthor,
-    insert,
-    update,
-    remove
+  get,
+  getById,
+  search,
+  byId,
+  byName,
+  byGender,
+  byAuthor,
+  insert,
+  update,
+  remove
 }
