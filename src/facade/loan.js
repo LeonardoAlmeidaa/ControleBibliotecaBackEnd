1a80be44dc825f18a217128da88e280f6b12bb3e
Leonardo Almeida <leoalmeida.pessoal@gmail.com>
2023-10-13 18:08:07 -0300

    CHORE: facade

--- src/facade/loan.js
+++ src/facade/loan.js
@@ -6,7 +6,7 @@ const tableName = "loan"
 const moment = require("moment")
 
 const get = async () => {
-  return await dbo.get(tableName)
+  return await dbo.getLoan(tableName)
 }
 
 const getById = async (id) => {
@@ -17,7 +17,7 @@ const getById = async (id) => {
 }
 
 const search = async (body) => {
-  if (!body || !Array.isArray(body) || body.length === 0)  {
+  if (!body || !Array.isArray(body) || body.length === 0) {
     return false
   }
   return await dbo.search(tableName, body)
@@ -27,7 +27,7 @@ const byId = async (body) => {
   if (!body) {
     return false
   }
-    return await dbo.byId(tableName, body, "id")
+  return await dbo.byId(tableName, body, "id")
 }
 
 const byIdUser = async (body) => {
@@ -37,18 +37,18 @@ const byIdUser = async (body) => {
   return await dbo.byId(tableName, body, "id_user")
 }
 
-const getByIdUser = async (id_user) => {
-  if (!id_user) {
+const getByIdUser = async (idUser) => {
+  if (!idUser) {
     return false
   }
-  return await dboLoan.getByIdUser(id_user, tableName)
+  return await dboLoan.getByIdUser(idUser, tableName)
 }
 
-const getByIdBook = async (id_book) => {
-  if (!id_book) {
+const getByIdBook = async (idBook) => {
+  if (!idBook) {
     return false
   }
-  return await dboLoan.getByIdBook(id_book, tableName)
+  return await dboLoan.getByIdBook(idBook, tableName)
 }
 
 const byIdBook = async (body) => {
@@ -64,29 +64,37 @@ const insert = async (object) => {
       abortEarly: false
     })
   } catch (error) {
-      const errors = error.details.map((el) => el.message)
-      return { errors }
+    console.log("ERRO SHERNOWS");
+    console.log(error);
+    const errors = error.details.map((el) => el.message)
+    return { errors }
   }
 
-  object.loanEnd = moment(object.loan_start).add("3", "days").format()
-  const param = [ { column: "id_book",  signal: '=', value: object.id_book } ]
+  object.loanEnd = moment(object.loanStart).add("3", "days").format()
+  const param = [{ column: "id_book", signal: '=', value: object.idBook }, {column: "status", signal: '=', value: '1'}]
   const oldData = await dbo.search(tableName, param)
 
+  console.log("OLD DATA");
+  console.log(oldData);
 
-  if (oldData.length < 1) {
+  if (oldData.data.length < 1) {
+    console.log("length");
     return await dbo.insert(object, tableName)
   }
 
   else {
-    for (let index = 0; index < oldData.length; index++) {
-      const element = oldData[index]
+    for (let index = 0; index < oldData.data.length; index++) {
+      const element = oldData.data[index]
 
-      if (moment(element.loanStart).format('yyyy-MM-DD') >= object.loan_start) {
-        return {errors: [{file: 'Erro', message: "asdasdasdasdasda" } ]}
+      console.log("OLD DATA - else");
+      console.log(element);
+
+      if (moment(element.loanStart).format('yyyy-MM-DD') >= object.loanStart) {
+        return { errors: [{ file: 'Erro', message: "Já existe um empréstimo ATIVO nesta data." }] }
       }
     }
   }
-  
+
   // return await dbo.insert(object, tableName)
 }
 
@@ -96,17 +104,17 @@ const update = async (object) => {
   }
   try {
     await validation.object.validateAsync(object, {
-      abortEarly:false
+      abortEarly: false
     })
   } catch (error) {
-      const errors = error.details.map((el) => el.message)
-      return { errors }
+    const errors = error.details.map((el) => el.message)
+    return { errors }
   }
   return await dbo.update(object, tableName)
 }
 
 const remove = async (id) => {
-  if(!id) {
+  if (!id) {
     return false
   }
   return await dbo.remove(id, tableName)
--- src/model/loan.js
+++ src/model/loan.js
@@ -1,10 +1,10 @@
 const Joi = require("joi")
 
 const object = Joi.object().keys({
-  id_user: Joi.number().required(),
-  id_book: Joi.number().required(),
-  loan_start: Joi.date().required(),
-  loan_end: Joi.date(),
+  idUser: Joi.number().required(),
+  idBook: Joi.number().required(),
+  loanStart: Joi.date().required(),
+  loanEnd: Joi.date(),
   status: Joi.number(),
   obs: Joi.string()
 })
